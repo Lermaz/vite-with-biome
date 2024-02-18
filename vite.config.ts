03d10ad9c09edde2d5477bb1573ab6e8aac95e4c
Lermaz <ilerma@lumston.com>
2024-02-18 03:09:40 -0700

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import biomePlugin from "vite-plugin-biome";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    biomePlugin({
+      mode: "check",
+      files: "src",
+      applyFixes: true,
+      failOnError: true,
+    }),
+  ],
+  test: {
+    globals: true,
+    // environment: "jsdom",
+    // setupFiles: "./src/test/setup.ts",
+    // you might want to disable it, if you don't have tests that rely on CSS
+    // since parsing CSS is slow
+    css: true,
+    coverage: {
+      reporter: ["text", "json", "html"],
+    },
+    reporters: ["html"],
+  },
+});
